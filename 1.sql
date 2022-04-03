7f99d71305957cf7d8841be08e213d1ccc3c1be5
latifaahu <elhaashabiyya8081@student.uns.ac.id>
2022-04-03 14:18:37 +0700

    Add query pemilik

--- 1.sql
+++ 1.sql
@@ -1,2 +1,16 @@
 Create Database APLIKASIPASAR;
 Use APLIKASIPASAR;
+
+CREATE TABLE PEMILIK (
+    Id INT NOT NULL,
+    Nama VARCHAR(50) NOT NULL,
+    Alamat VARCHAR(100) NOT NULL,
+    Nik VARCHAR(16) NOT NULL,
+    No_wa VARCHAR(13),
+    No_telp VARCHAR(13),
+    Created_by VARCHAR(50),
+    Edited_by VARCHAR(50),
+    Created_at DATETIME,
+    Edited_at DATETIME,
+    PRIMARY KEY (Id)
+)
